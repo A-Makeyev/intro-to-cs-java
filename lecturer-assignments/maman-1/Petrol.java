86b4e1b62106a77fb274c7879bc2f4149e18e3b4
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2020-03-29 12:15:38 +0300

    added first assignment

---
+++ lecturer-assignments/maman-1/Petrol.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Petrol {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        final double MAX = 100,
+                     MILE_TO_KM = 1.609,
+                     LITRES_PER_GALLON = 3.785;
+
+
+        System.out.print("Please enter the car's petrol consumption measured in miles/gallon: ");
+        double milesPerGallon = scan.nextDouble();
+
+        if (milesPerGallon <= 0) {
+            System.err.println("Miles Per Gallon cannot be zero or a negative number.");
+            System.exit(1);
+        }
+
+
+        /*
+         * 1 mile = 1.609km
+         * 1 gallon = 3.785 litres
+         *
+         * 20 miles per 1 gallon => (20 * 1.609) / 3.785 => 8.5 litres per 1 km
+         *
+         */
+
+
+        double litresTo100km = 100 / ((milesPerGallon * MILE_TO_KM) / LITRES_PER_GALLON);
+
+
+        double afterRounding = Math.round(litresTo100km * MAX) / (MAX * 1.0);
+        System.out.println("The car's petrol consumption converted to litres/100km is: " + afterRounding);
+
+    }
+}
---
+++ lecturer-assignments/maman-1/Trapezoid.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Trapezoid {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Please enter the left point coordinates of the base followed by its length: ");
+        int AB_left_x = scan.nextInt();
+        int AB_left_y = scan.nextInt();
+        int AB_base = scan.nextInt();
+
+        System.out.print("Please enter the left point coordinates of the base followed by its length: ");
+        int DC_left_x = scan.nextInt();
+        int DC_left_y = scan.nextInt();
+        int DC_base = scan.nextInt();
+
+
+        // Finding the length of DC
+
+        double DC_length = Math.sqrt(Math.pow((AB_left_x - DC_left_x), 2) + Math.pow((AB_left_y - DC_left_y), 2));
+
+
+        // Finding the length of CB
+
+        double  DC_right_x = DC_left_x + DC_base,
+                DC_right_y = DC_left_y;
+
+        double AB_right_x = AB_left_x + AB_base,
+               AB_right_y = AB_left_y;
+
+        double CB_length = Math.sqrt(Math.pow((DC_right_x - AB_right_x), 2) + Math.pow((DC_right_y - AB_right_y), 2));
+
+
+        // Calculating the height, area and perimeter of the trapezoid
+
+        double height, area, perimeter;
+
+        height = DC_left_y - AB_left_y;
+
+        area = (height * (AB_base + DC_base)) / 2;
+
+        // Perimeter = AB_base + DC_base + DC_length + CB_length
+        perimeter = Math.round((AB_base + DC_base + DC_length + CB_length) * 100) / (100 * 1.0);
+
+
+        System.out.println("\nThe area of the trapezoid is: " + area);
+        System.out.println("The perimeter of the trapezoid is: " + perimeter);
+
+    }
+}
